6d25e938ae83bb6e4458661bec280087cc7e4fe0
Balamurugan Renganathan <krbmurugan@users.noreply.github.com>
2018-04-16 14:25:57 -0600

    additional tests added

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -2,15 +2,31 @@ package com.devopsbuddy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devopsbuddy.web.i18n.I18nService;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18nService I18nService;
+	 
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception{
+		String expectedResult ="Home page template22";
+		String messageId = "index.main.callout";
+		
+		String actual = I18nService.getMessage(messageId);
+		Assert.assertEquals("Actual and expected dont match",expectedResult, actual);
+		
 	}
 
 }
+ 
